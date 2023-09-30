a22d85448ddc86a19c73a142b5e47b5abf048ebb
shijieli94 <146414271+shijieli94@users.noreply.github.com>
2023-09-30 13:33:37 +1000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="legacy_package",
+    version="0.1.0",
+    description="legacy packages",
+    url="https://github.com/shijieli94/legacy-package",
+    license="MIT",
+    packages=find_packages(),
+    include_package_data=True,
+)
